ad0c86b950485b8112925acd1a2ff9db7cadeffa
Ivars Žeibe <Ivars_Zeibe@hotmail.com>
2022-10-25 13:06:34 +0300

    Refactoring

--- script.js
+++ script.js
@@ -1,57 +1,37 @@
-function scrollInto(id) {
-    document.getElementById(id).scrollIntoView(true);
-}
-
-let el = document.getElementsByTagName("h1")[0];
-
 function startDoomsday() {
+    let el = document.getElementsByTagName("h1")[0];
+
     el.style.fontSize = "500px";
+    el.style.backgroundImage = "none";
+    document.body.style.backgroundImage = "none";
+
     window.scrollTo(0, 0);
-    let x = window.setInterval(changeColor, 50);
-    let y = window.setInterval(rotate, 0);
+    window.setInterval(() => changeColor(el), 50);
+
+    let rotation = { value: 0 };
+    window.setInterval(() => rotate(el, rotation), 0);
 }
 
+function changeColor(el) {
+    el.style.color = nextColor(el.style.color, ["red", "blue", "yellow", "green"]);
+    el.style.backgroundColor = nextColor(el.style.backgroundColor, ["pink", "springgreen"]);
+    document.body.style.backgroundColor = nextColor(document.body.style.backgroundColor, ["yellow", "black"]);    
+}
 
-function changeColor() {
-    switch(el.style.color) {
-        case "red":
-            el.style.color = "blue";
-            break;
-        case "blue":
-            el.style.color = "yellow";
-            break;
-        case "yellow":
-            el.style.color = "green";
-            break;
-        default:
-            el.style.color = "red";
-            break;
+function nextColor(currentColor, allColors) {
+    let currentColorIndex = allColors.indexOf(currentColor);
+    if (currentColorIndex + 1 < allColors.length) {
+        return allColors[currentColorIndex + 1];
+    } else {
+        return allColors[0];
     }
-    
-    switch(el.style.color) {
-        case "red":
-            el.style.backgroundColor = "green";
-            break;
-        default:
-            el.style.backgroundColor = "red";
-            break;
-    }
-    
-    switch(document.body.style.backgroundColor) {
-        case "yellow":
-            document.body.style.backgroundColor = "black";
-            break;
-        default:
-            document.body.style.backgroundColor = "yellow";
-            break;
-    }
-    
 }
-let rotation = 0;
-function rotate() {
-    el.style.transform = `rotate(-${rotation}deg)`;
-    rotation++;
+
+function rotate(el, rotation) {
+    el.style.transform = `rotate(-${rotation.value}deg)`;
+    rotation.value++;
 }
+
 
 //https://www.w3schools.com/howto/howto_js_slideshow.asp
 let slideIndex = 1;
@@ -59,55 +39,56 @@ showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    showSlides(slideIndex += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-
-
-function openNavigation() {
-    let nav = document.getElementById("nav");
-    nav.style.top = "0";
-    if (nav.className === "nav") {
-        nav.className += " responsive";
-    } else {
-        nav.className = "nav";
+    let i;
+    let slides = document.getElementsByClassName("my-slides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1;
     }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].classList.add("active");
 }
 
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
+document.getElementById("navigationButton").addEventListener("click", () => {
     let nav = document.getElementById("nav");
-    if (prevScrollpos > currentScrollPos) {
+    if (!nav.classList.contains("open")) {
+        nav.classList.add("open");
         nav.style.top = "0";
     } else {
-        nav.style.top = `-${document.getElementById("nav").offsetHeight - 5}px`;
-        nav.className = "nav";
+        nav.classList.remove("open");
     }
-    prevScrollpos = currentScrollPos;
-}
-function hideNavigation() {    
-    nav.style.top = `-${document.getElementById("nav").offsetHeight - 5}px`;
-    nav.className = "nav";
+});
+
+// Shows and hides navigation on scroll
+var previousScrollPosition = window.pageYOffset;
+window.onscroll = () => {
+    var currentScrollPos = window.pageYOffset;
+
+    let nav = document.getElementById("nav");
+    nav.classList.remove("open");
+
+    if (previousScrollPosition > currentScrollPos) {
+        nav.style.top = "0";
+    } else {
+        nav.style.top = `-${nav.offsetHeight - 5}px`;
+    }
+    previousScrollPosition = currentScrollPos;
 }

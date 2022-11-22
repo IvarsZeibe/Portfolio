4044adf4a8f5d95fbe9492af6948ebc92a288689
Ivars Žeibe <Ivars_Zeibe@hotmail.com>
2022-11-22 16:30:34 +0200

    Refactoring

--- script.js
+++ script.js
@@ -87,13 +87,13 @@ window.onscroll = () => {
 
     if (previousScrollPosition > currentScrollPos) {
         nav.style.top = "0";
-    } else {
+    } else if (currentScrollPos > parseFloat(window.getComputedStyle(nav, null).height)) {
         nav.style.top = `-${nav.offsetHeight - 5}px`;
     }
     previousScrollPosition = currentScrollPos;
 }
 
-
+// Flappy bird easter egg game
 class Hitbox {
     constructor(shape, x, y, width, height) {
         this.x = x;
@@ -192,194 +192,214 @@ class Obstacle {
         );
     }
 }
-
-let canvas = document.getElementById("hero");
-let context = canvas.getContext("2d");
-let birdHitbox = Hitbox.createCircle(100, 400, 20)
-let hasJumped = false;
-let velocity = {x: 10, y: 0};
-let dotHitbox = Hitbox.createRectangle(-1, -1, 0, 0);
-let isEasterEggActive = false;
-let isEasterEggStarted = false;
-let starPosition = {x: 100, y: 400};
-let obstacles = [];
-let currenTick = 0;
-let obstacleSpawnedTick = 0;
-const obstacleSpawningCooldown = 100;
-let vh = 0;
-let vw = 0;
-let ceilingHeight = 0;
-let screenWidth = 0;
-let ratio = 0;
-let score = 0;
-let highestScore = 0;
-let lastResetTick = 0;
-document.fonts.ready.then(redraw);
-
-addEventListener("resize", redraw);
-
-function redraw() {
-    context.clearRect(0, 0, canvas.width, canvas.height); 
-    
-    ratio = Math.ceil(window.devicePixelRatio);
-    canvas.width = parseFloat(window.getComputedStyle(canvas, null).width) * ratio;
-    canvas.height = parseFloat(window.getComputedStyle(canvas, null).height) * ratio;
-    context.setTransform(ratio, 0, 0, ratio, 0, 0);
-
-    vw = canvas.width / ratio / 100;
-    vh = canvas.height / ratio / 100;
-    
-    let navHeight = parseFloat(window.getComputedStyle(document.getElementById("nav"), null).height);
-    ceilingHeight = navHeight + 4*vh;
-    starPosition.x = 10*vh;
-    starPosition.y = (canvas.height - ceilingHeight) / ratio / 2;
-
-    const fontSize = 10*vw;
-    context.font = `bold 10vw TimesNewRomanBold`;
-    if (isEasterEggActive) {
-        context.fillStyle = "#add8e644";
-    } else {
-        context.fillStyle = "lightblue"; // #add8e6
-    }
-
-    let text = context.measureText("Ivars Že");
-    let text2 = context.measureText(".");
-    let text3 = context.measureText("i");
-    
-    const textPaddingLeft = 5*vw;
-    const textPaddingTop = 20*vh;
-
-    dotHitbox.width = text2.width * 0.8;
-    dotHitbox.height = text2.width * 0.9;
-    dotHitbox.x = textPaddingLeft + text.width + (text3.width - text2.width)/2 + text2.width * 0.12 + dotHitbox.width / 2;
-    dotHitbox.y = textPaddingTop - 8 * vw + text2.width * 4 + text2.width * 0.3 + dotHitbox.height / 2;
-
-    birdHitbox.radius = dotHitbox.width/2;
-    context.fillText("Ivars Žeibe", textPaddingLeft, fontSize + textPaddingTop);
-    context.fillStyle = "#81c1d6";
-    context.save();
-    context.rect(dotHitbox.x - dotHitbox.width/2, dotHitbox.y - dotHitbox.height/2, dotHitbox.width, dotHitbox.height);
-    context.clip();
-    context.fillText("i", textPaddingLeft + text.width, fontSize + textPaddingTop);
-    context.restore();
-
-    if (isEasterEggActive || isEasterEggStarted) {        
-        // clears dot
-        context.clearRect(dotHitbox.x - dotHitbox.width/2, dotHitbox.y - dotHitbox.height/2, dotHitbox.width, dotHitbox.height);
-
-        context.font = `bold 8${vh < vw ? "vh" : "vw"} TimesNewRomanBold`;
-        context.fillText(`Score: ${score}|${highestScore}`, textPaddingLeft, canvas.height / ratio - 15*vh);
+const bird = {
+    hitbox: Hitbox.createCircle(100, 400, 20),
+    velocity: {x: 10, y: 0},
+    hasJumped: true,
+    jump: function() {
+        if (game.isActive && !this.hasJumped) {
+            this.velocity.y = -6*screen.vh;
+            this.hasJumped = true;
+        }
     }
 }
 
-function updateGame() {
-    currenTick++;
-    redraw();
-    // Transparent overlay
-    // context.fillStyle = "#00000044"
-    // context.fillRect(dotHitbox.x - dotHitbox.width/2, dotHitbox.y - dotHitbox.height/2, dotHitbox.width, dotHitbox.height);
-
-    if (isEasterEggStarted) {
-        let c = Math.sqrt(Math.pow(starPosition.x - birdHitbox.x, 2) + Math.pow(starPosition.y - birdHitbox.y, 2));
-        let speed = 0.5 * Math.sqrt(vw*vw + vh*vh);
-        birdHitbox.x += (starPosition.x - birdHitbox.x) / c * speed;
-        birdHitbox.y += (starPosition.y - birdHitbox.y) / c * speed;
-        if (Math.abs(starPosition.x - birdHitbox.x) + Math.abs(starPosition.x - birdHitbox.x) < speed*2) {
-            isEasterEggStarted = false;
-            isEasterEggActive = true;
-        }
+const screen = {
+    canvas: document.getElementById("hero"),
+    vh: 0,
+    vw: 0,
+    ceilingHeight: 0,
+    width: 0,
+    height: 0,
+    ratio: 1,
+    dotHitbox: Hitbox.createRectangle(-1, -1, 0, 0),
+    update: function() {
+        let context = this.canvas.getContext("2d");
         
-        // draw dot
-        context.fillStyle = "lightblue";
-        context.beginPath();
-        context.arc(birdHitbox.x, birdHitbox.y, birdHitbox.radius, 0, 2 *Math.PI);
-        context.fill();
+        // set canvas size
+        ratio = Math.ceil(window.devicePixelRatio);
+        this.canvas.width = parseFloat(window.getComputedStyle(this.canvas, null).width) * ratio;
+        this.canvas.height = parseFloat(window.getComputedStyle(this.canvas, null).height) * ratio;
+        context.setTransform(ratio, 0, 0, ratio, 0, 0);
+        
+        this.width = this.canvas.width / ratio;
+        this.height = this.canvas.height / ratio;
+        this.vw = this.width / 100;
+        this.vh = this.height / 100;
+        
+        let navHeight =  parseFloat(window.getComputedStyle(document.getElementById("nav"), null).height);
+        this.ceilingHeight = navHeight;
+        
+        
+        const textPaddingLeft = 5*this.vw;
+        const textPaddingTop = 20*this.vh;
+    
+        context.font = `bold 10vw TimesNewRomanBold`;
+        context.fillStyle = this.isActive ? "#add8e644" : "lightblue"; // lightblue == #add8e6
+        let text = context.measureText("Ivars Že");
+        let text2 = context.measureText(".");
+        let text3 = context.measureText("i");
+        
+        this.dotHitbox.width = text2.width * 0.8;
+        this.dotHitbox.height = text2.width * 0.9;
+        this.dotHitbox.x = textPaddingLeft + text.width + (text3.width - text2.width)/2 + text2.width * 0.12 + this.dotHitbox.width / 2;
+        this.dotHitbox.y = textPaddingTop - 8 * this.vw + text2.width * 4 + text2.width * 0.3 + this.dotHitbox.height / 2;
+    
+        bird.hitbox.radius = this.dotHitbox.width/2;
+        game.startPosition.x = 10*this.vh;
+        game.startPosition.y = (this.height - this.ceilingHeight) / 2;
+        
     }
+};
+const game = {
+    isActive: false,
+    isIntroActive: false,
+    currenTick: 0,
+    lastResetTick: 0,
+    obstacles: [],
+    obstacleSpawnedTick: 0,
+    obstacleSpawningCooldown: 100,
+    score: 0,
+    highestScore: 0,
+    startPosition: {x: 0, y: 0},
+    reset: function() {
+        bird.hitbox.x = this.startPosition.x;
+        bird.hitbox.y = this.startPosition.y;
+        bird.velocity.y = 0;
+        bird.velocity.x = 10;
+        this.obstacles = [];
+        this.score = 0;
+        this.lastResetTick = this.currenTick;
+        this.obstacleSpawnedTick = this.lastResetTick;
+    },
+    redraw: function() {
+        let context = screen.canvas.getContext("2d");
+        // clear canvas
+        context.clearRect(0, 0, screen.width, screen.height); 
+        
+        const fontHeight = 10*screen.vw;
+        context.font = `bold 10vw TimesNewRomanBold`;
+        context.fillStyle = this.isActive ? "#add8e644" : "lightblue"; // lightblue == #add8e6
 
-    if (isEasterEggActive) {
-        // draw ceiling
-        drawCeiling(canvas.width / ratio, ceilingHeight)
+        const textPaddingLeft = 5*screen.vw;
+        const textPaddingTop = 20*screen.vh;
 
-        birdHitbox.y += velocity.y * 0.1;
-        velocity.y += 0.15*vh;
-        // draw dot
-        context.fillStyle = "lightblue";
-        context.beginPath();
-        context.arc(birdHitbox.x, birdHitbox.y, birdHitbox.radius, 0, 2 *Math.PI);
-        context.fill();
+        let text = context.measureText("Ivars Že");       
+        
+        // draw title
+        context.fillText("Ivars Žeibe", textPaddingLeft, fontHeight + textPaddingTop);
+        context.fillStyle = "#81c1d6";
+        context.save();
+        context.rect(screen.dotHitbox.x - screen.dotHitbox.width/2, screen.dotHitbox.y - screen.dotHitbox.height/2, screen.dotHitbox.width, screen.dotHitbox.height);
+        context.clip();
+        context.fillText("i", textPaddingLeft + text.width, fontHeight + textPaddingTop);
+        context.restore();
 
-        hasJumped = false;
-        if (currenTick - obstacleSpawnedTick > obstacleSpawningCooldown) {
-            let obstacleWidth = 4*vw;
-            let obstacle = new Obstacle(
-                canvas.width / ratio + obstacleWidth/2,
-                (canvas.height / ratio - 10*vh) / 2 + ceilingHeight,
-                obstacleWidth,
-                canvas.height / ratio - 10*vh,
-                0, 0.9);
-            obstacles.push(obstacle); 
-            obstacleSpawnedTick = currenTick;
-            score = Math.max(0, Math.ceil((currenTick - lastResetTick - (canvas.width / ratio + obstacleWidth / 2 - birdHitbox.x) / (0.4*vw)) / obstacleSpawningCooldown));
-            if (score > highestScore) {
-                highestScore = score;
+        if (this.isActive) {        
+            // clears dot above i letter
+            context.clearRect(screen.dotHitbox.x - screen.dotHitbox.width/2, screen.dotHitbox.y - screen.dotHitbox.height/2, screen.dotHitbox.width, screen.dotHitbox.height);
+
+            // write score
+            context.font = `bold 8${screen.vh < screen.vw ? "vh" : "vw"} TimesNewRomanBold`;
+            context.fillText(`Score: ${this.score}|${this.highestScore}`, textPaddingLeft, screen.height - 15*screen.vh);
+            
+            // draw bird
+            context.fillStyle = "lightblue";
+            context.beginPath();
+            context.arc(bird.hitbox.x, bird.hitbox.y, bird.hitbox.radius, 0, 2*Math.PI);
+            context.fill();
+            
+            // draw ceiling
+            context.fillStyle = "#000000";
+            context.fillRect(0, 0, screen.width, screen.ceilingHeight);
+
+            // draw obstacles
+            this.obstacles.forEach(o => o.draw(context));
+        }
+    },
+    update: function() {
+        this.redraw();
+        if (this.isIntroActive) {
+            let distanceFromStartToBird = Math.sqrt(Math.pow(this.startPosition.x - bird.hitbox.x, 2) + Math.pow(this.startPosition.y - bird.hitbox.y, 2));
+            let speed = 0.5 * Math.sqrt(screen.vw*screen.vw + screen.vh*screen.vh);
+            bird.hitbox.x += (this.startPosition.x - bird.hitbox.x) / distanceFromStartToBird * speed;
+            bird.hitbox.y += (this.startPosition.y - bird.hitbox.y) / distanceFromStartToBird * speed;
+            if (Math.abs(this.startPosition.x - bird.hitbox.x) + Math.abs(this.startPosition.x - bird.hitbox.x) < speed*2) {
+                this.isIntroActive = false;
             }
+        } else if (this.isActive) {
+            this.currenTick++;    
+            bird.hitbox.y += bird.velocity.y * 0.1;
+            bird.velocity.y += 0.15*screen.vh;
+            bird.hasJumped = false;
+            const obstacleMovementSpeed = -0.4*screen.vw;
+    
+            let obstacleWidth = 4*screen.vw;
+            if (this.currenTick - this.obstacleSpawnedTick > this.obstacleSpawningCooldown) {
+                let obstacle = new Obstacle(
+                    screen.width + obstacleWidth/2,
+                    (screen.height - 10*screen.vh) / 2 + screen.ceilingHeight,
+                    obstacleWidth,
+                    screen.height - 10*screen.vh,
+                    0, 0.9);
+                this.obstacles.push(obstacle); 
+                this.obstacleSpawnedTick = this.currenTick;
+            }
+            let startingOffset = (screen.width + obstacleWidth - bird.hitbox.x) / Math.abs(obstacleMovementSpeed);
+            this.score = Math.max(0, Math.floor((this.currenTick - this.lastResetTick - startingOffset) / this.obstacleSpawningCooldown));
+
+            if (this.score > this.highestScore) {
+                this.highestScore = this.score;
+            }
+
+            this.obstacles.forEach(o => {
+                o.move(obstacleMovementSpeed, 0);
+            });
+            this.obstacles = this.obstacles.filter(o => o.x >= -o.width / 2);
+            if (bird.hitbox.y - bird.hitbox.radius < screen.ceilingHeight || bird.hitbox.y + bird.hitbox.radius > screen.height) {
+                this.reset();
+            } else {
+                this.obstacles.some(o => {
+                    if (o.isCollidingWith(bird.hitbox)) {
+                        this.reset();
+                        return true;
+                    }
+                    return false;
+                })
+            }
+            
         }
-        obstacles.forEach(o => {
-            o.move(-0.4*vw, 0);
-            o.draw(context);
-        });
-        obstacles = obstacles.filter(o => o.x >= -o.width / 2);
-        if (birdHitbox.y - birdHitbox.radius < ceilingHeight || birdHitbox.y + birdHitbox.radius > canvas.height / ratio) {
-            resetFlappyBird();
-        } else {
-            obstacles.some(o => {
-                if (o.isCollidingWith(birdHitbox)) {
-                    resetFlappyBird();
-                    return true;
-                }
-                return false;
-            })
-        }
-        
     }
-}
-function drawCeiling(width, height) {
-    context.fillStyle = "#000000";
-    context.fillRect(0, 0, width, height);
 }
 document.addEventListener("keydown", (e) => {
     if (e.key == " " && e.target == document.body) {
         e.preventDefault();
-        jump();
+        bird.jump();
     }
     else if (e.key == "r" && e.target == document.body) {
         e.preventDefault();
-        resetFlappyBird();
+        game.reset();
     }
 });
-function resetFlappyBird() {
-    birdHitbox.x = starPosition.x;
-    birdHitbox.y = starPosition.y;
-    velocity.y = 0;
-    velocity.x = 10;
-    obstacles = [];
-    score = 0;
-    lastResetTick = currenTick;
-}
 document.addEventListener("click", (e) => {
-    let rect = canvas.getBoundingClientRect();
-    if (!isEasterEggStarted && !isEasterEggActive && dotHitbox.isCollidingWithPoint(e.clientX - rect.left, e.clientY - rect.top)) {
-        birdHitbox.x = dotHitbox.x;
-        birdHitbox.y = dotHitbox.y;
-        isEasterEggStarted = true;
-        window.setInterval(updateGame, 10);
+    let rect = screen.canvas.getBoundingClientRect();
+    if (!game.isActive && screen.dotHitbox.isCollidingWithPoint(e.clientX - rect.left, e.clientY - rect.top)) {
+        bird.hitbox.x = screen.dotHitbox.x;
+        bird.hitbox.y = screen.dotHitbox.y;
+        game.isActive = true;
+        game.isIntroActive = true;
+        console.log(game.isIntroActive);
+        window.setInterval(() => game.update(), 10);
     }
 });
-
-canvas.addEventListener("mousedown", jump);
-function jump() {
-    if (isEasterEggActive && !hasJumped) {
-        velocity.y = -6*vh;
-        hasJumped = true;
-    }
-}
+screen.canvas.addEventListener("mousedown", () => {
+    bird.jump(); // needs to be wrapped in a function because otherwise the function doesn't belong to bird
+});
+addEventListener("resize", () => {
+    screen.update();
+    game.redraw();
+});
+screen.update();
+document.fonts.ready.then(() => {
+    game.redraw();
+});

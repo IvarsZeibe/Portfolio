263a53997ac65338c17c7718b9a2e2785586847f
Ivars Žeibe <Ivars_Zeibe@hotmail.com>
2022-12-07 17:30:31 +0200

    Updated structure

--- README.md
+++ README.md
@@ -2,9 +2,6 @@
 
 Showcase website for learning purposes. Easter egg flappy bird can be started by clicking differently colored dot in Ivars Zeibe.
 
-Link to website without backend (message sending doesn't work): <br />
-https://ivarszeibe.github.io/Portfolio/
-
-Link to website with backend: <br />
+Link to website: <br />
 http://www.iz.id.lv/
 
--- public/messages.php
+++ public/messages.php
@@ -2,7 +2,7 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["message"])) {
         echo "Message sent";
-        $myfile = fopen("messages.txt", "a");
+        $myfile = fopen("../messages.txt", "a");
         fwrite($myfile, "Email: ".$_POST["email"].", full name: ".$_POST["name"].", message: ".$_POST["message"]."\n");    
     } else {
         echo "Invalid form data";

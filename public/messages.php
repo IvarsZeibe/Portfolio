eee6e542406967cad187735acc394cb7fdf67210
IvarsZeibe <ivars_zeibe@hotmail.com>
2022-12-13 10:31:43 +0200

    Added text limit

--- public/messages.php
+++ public/messages.php
@@ -1,6 +1,7 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["message"])) {
+    if (isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["message"]) &&
+    strlen($_POST["name"]) <= 50 && strlen($_POST["email"]) <= 50 && strlen($_POST["message"]) <= 300) {
         echo "Message sent";
         $myfile = fopen("../messages.txt", "a");
         fwrite($myfile, "Email: ".$_POST["email"].", full name: ".$_POST["name"].", message: ".$_POST["message"]."\n");    
